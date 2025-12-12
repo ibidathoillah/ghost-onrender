0b0158c4a7047e25ccf59619f201f44aafe91a7a
ibid <ibidathoillah@gmail.com>
2025-12-12 15:38:36 +0700

    update

--- init.sh
+++ init.sh
@@ -5,5 +5,9 @@ export database__connection__filename=/var/lib/ghost/content/data/ghost.db
 export server__host=0.0.0.0
 export server__port=${PORT:-2368}
 export url=${RENDER_EXTERNAL_URL:-http://localhost:${server__port}}
+mkdir -p /var/lib/ghost/content/data
+mkdir -p /var/lib/ghost/content/logs
+mkdir -p /var/lib/ghost/content/public
+mkdir -p /var/lib/ghost/content/settings
 cd /var/lib/ghost
 node current/index.js
